f6fb41d4dc05e3b3c3c5dab1d9bedcda5a90a7a8
maryam-muneer <maryammuneer000@gmail.com>
2025-11-28 12:13:47 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let button = document.getElementById('btn');
+let inputField = document.getElementById('inputName');
+let bgDiv = document.getElementById('bg');
+
+button.addEventListener('dblclick', function() {
+    console.log("Button was doubbled clicked!");
+});
+inputField.addEventListener('input', function() {
+    console.log(inputField.value);
+});
+bgDiv.addEventListener('mouseover', function() {
+    bgDiv.style.backgroundColor = "pink";
+    console.log("Mouse is over the div!");
+});                                                        setTimeout(function() {
+    console.log("3 seconds have passed!");            
+}, 3000);
+//  setInterval(function() {
+//     console.log(new Date().toLocaleTimeString());
+// }, 1000);
+inputField.addEventListener("keypress", function(event) {
+    console.log("You pressed: " + event.key);
+});
+
+
+                                          
